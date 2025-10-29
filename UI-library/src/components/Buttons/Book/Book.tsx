6c533912dec16c1976c92223f26a17e71184e2d3
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-10-29 13:12:56 +0100

    added styling to boka btn and fixed import issue for Blipp image

--- UI-library/src/components/Buttons/Book/Book.tsx
+++ UI-library/src/components/Buttons/Book/Book.tsx
@@ -1,5 +1,18 @@
-const Book = () => {
-  return <div></div>;
+const Book = (
+  handleBook = () => {
+    console.log("hi");
+  }
+) => {
+  return (
+    <div>
+      <button
+        className="w-[138px] h-[48px] bg-gradient-to-tr from-[#FF0000] via-[#E9006D] to-[#D200DA] rounded-md text-white cursor-pointer"
+        onClick={handleBook}
+      >
+        Boka
+      </button>
+    </div>
+  );
 };
 
 export default Book;
